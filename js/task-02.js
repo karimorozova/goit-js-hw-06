31f064987ba2d620302026414278db226f92e4be
karine-morozova <morozova_karine@ukr.net>
2022-01-10 17:53:02 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,12 @@
+/* 
+Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
+Добавит название ингредиента как его текстовое содержимое.
+Добавит элементу класс item.
+После чего вставит все <li> за одну операцию в список ul.ingredients
+*/
+
+
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +15,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsRef = document.querySelector("#ingredients");
+console.log(ingredientsRef);
+
+for(const el of ingredients) {
+  const item = document.createElement('li');
+  item.classList.add("item");
+  item.textContent = el;
+  console.log(item.textContent);
+  console.log(item)
+  ingredientsRef.append(item)
+}
