76f5453703cccec4c4ddae449d78609cddc703e5
karine-morozova <morozova_karine@ukr.net>
2022-01-13 09:08:57 +0200

    Update task 6 with ternary operator

--- js/task-06.js
+++ js/task-06.js
@@ -14,12 +14,14 @@ inputRef.addEventListener('blur', onInputBlur);
 
 function onInputBlur(event) {
     console.log(inputRef.value.length);
-    if (inputRef.value.length < 6) {
+
+     inputRef.value.length < 6 ? inputRef.classList.add('invalid') : inputRef.classList.add('valid');
+    // if (inputRef.value.length < 6) {
         
-        inputRef.classList.add('invalid');
-    } else {
-        inputRef.classList.add('valid');
-    }
+    //     inputRef.classList.add('invalid');
+    // } else {
+    //     inputRef.classList.add('valid');
+    // }
        
     
     
